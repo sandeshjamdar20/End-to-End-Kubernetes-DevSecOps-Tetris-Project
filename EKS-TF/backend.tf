b24c35b5c8c402824712e06ee244565c7ddecff8
sandeshjamdar20 <sandeshjamdar51@gmail.com>
2026-07-16 16:40:11 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dev-aman-tf-bucket"
+    bucket       = "jamdarwadi2026-2"
     region       = "us-east-1"
     key          = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     use_lockfile = true
